ca4923c598be159dfb13036ba347a0b23133cf5e
KonstantinShemyak <konstantin@shemyak.com>
2013-01-03 13:49:52 +0200

    Minor refactoring

--- markOSMBridges.py
+++ markOSMBridges.py
@@ -14,11 +14,12 @@ from graphBuilder import HighwayGraphBuilder
 from markWays import markCutEdges
 from time import gmtime, strftime	# Only for print_timing()
 
+# Only for watching the progress, can be turned off:
 def print_timing(line):
   timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
   sys.stderr.write("%s %s\n" % (timestamp, line))
 
-def main(sourceFileName):
+def main(source_file):
 
   print_timing('Started')
   parser = make_parser()
@@ -26,7 +27,7 @@ def main(sourceFileName):
   gr = graph()
   parser.setContentHandler(HighwayGraphBuilder(gr))
   # Looks like building the graph is about linear in time!
-  parser.parse(sourceFileName)
+  parser.parse(source_file)
   print_timing('Graph built, %d nodes, %d edges' % (len(gr.nodes()), len(gr.edges()) / 2))
 
   # Find cut edges and collect them to a set
@@ -34,7 +35,7 @@ def main(sourceFileName):
   print_timing("Cut edges: %d" % len(cutEdges))
 
   # Do the second pass and output changed highways
-  markCutEdges(sourceFileName, cutEdges)
+  markCutEdges(source_file, cutEdges)
   print_timing('Marked')
 
 if __name__ == "__main__":
